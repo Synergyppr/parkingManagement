d3faed727efbae609e743535557e9df87f92cd58
Angie Mercado <wtelemaco@synergyppr.com>
2025-06-19 09:20:16 -0400

    Adding the route

---
+++ src/app/api/login/route.tsx
@@ -0,0 +1,10 @@
+import { PostContentData } from "@/app/lib/apiFunctions";
+import { NextResponse } from "next/server";
+
+// /api/ValetParking/Login
+export async function POST(req: Request) {
+  const data = await req.json();
+
+  const response = await PostContentData("Login", data);
+  return NextResponse.json(response);
+}
